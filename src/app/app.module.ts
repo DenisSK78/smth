421812e0a0ed38327b7ea36876c038ffcec24fd8
Denis Kalinin <k-d-s@mail.ru>
2018-12-30 11:36:48 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { AuthenticationComponent } from './authentication/authentication.component';
+import { UnitsComponent } from './units/units.component';
+import { RegistarationComponent } from './registration/registaration.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthenticationComponent,
+    UnitsComponent,
+    RegistarationComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: 'authentication', component: AuthenticationComponent},
+      {path: 'units', component: UnitsComponent},
+      {path: 'registration', component: RegistarationComponent},
+      {path: '', redirectTo: 'authentication', pathMatch: 'full' }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+}
