d0606184ac8dc1fcd5bba3a023867d6894697844
Denis Kalinin <k-d-s@mail.ru>
2018-12-31 17:45:23 +0300

    validation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { RegistrationComponent } from './registration/registration.component';
 import {AuthService} from './share/auth/auth.service';
 import {HttpClientModule} from '@angular/common/http';
 import {FormsModule} from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -18,6 +19,7 @@ import {FormsModule} from '@angular/forms';
     RegistrationComponent
   ],
   imports: [
+    ReactiveFormsModule,
     FormsModule,
     HttpClientModule,
     BrowserModule,
--- src/app/authentication/authentication.component.ts
+++ src/app/authentication/authentication.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {AuthService} from '../share/auth/auth.service';
 import {Router} from '@angular/router';
 import {AuthModel} from '../share/auth/auth.model';
+import {JwtModel} from '../share/jwt/jwt.model';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-authentication',
@@ -10,17 +12,54 @@ import {AuthModel} from '../share/auth/auth.model';
 })
 export class AuthenticationComponent implements OnInit {
 
-  authentication: AuthModel;
+  public jwtModel: JwtModel;
+  form: FormGroup;
 
-  constructor(private router: Router, private authService: AuthService) { }
+  constructor(private router: Router,
+              private authService: AuthService,
+              private fb: FormBuilder
+              ) { }
 
   ngOnInit() {
-      this.authentication = new AuthModel();
+      this.initForm();
   }
 
   onAuth() {
-    this.authService.authRequest(this.authentication)
-      .subscribe(data => console.log(data));
+    this.authService.authRequest( this.form.getRawValue())
+      .subscribe(data => {
+        this.jwtModel = data;
+        // console.log(this.jwtModel);
+      });
   }
 
+  private initForm() {
+    this.form = this.fb.group({
+      password: ['', [
+        Validators.required
+      ]
+      ],
+      username: ['', [
+        Validators.required, Validators.email
+      ]
+      ]
+    });
+  }
+
+  onSubmit() {
+    const controls = this.form.controls;
+
+    if (this.form.invalid) {
+      Object.keys(controls)
+        .forEach(controlName => controls[controlName].markAsTouched());
+      return;
+    }
+    this.onAuth();
+  }
+
+  isControlInvalid(controlName: string): boolean {
+    const control = this.form.controls[controlName];
+    return control.invalid && control.touched;
+  }
+
+// localStorage.setItem('id_token', authResult.idToken);
 }
